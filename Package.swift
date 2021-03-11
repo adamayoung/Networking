1d5327d2d59de0a153857c801820a2d83b4a0d54
Adam Young <me@adam-young.co.uk>
2021-03-11 15:43:21 +0000

    Refactored build

--- Package.swift
+++ Package.swift
@@ -14,10 +14,6 @@ let package = Package(
         .library(name: "Networking", targets: ["Networking"])
     ],
 
-    dependencies: [
-        .package(url: "https://github.com/realm/SwiftLint.git", from: "0.43.0")
-    ],
-
     targets: [
         .target(name: "Networking", dependencies: []),
         .testTarget(name: "NetworkingTests", dependencies: ["Networking"])
